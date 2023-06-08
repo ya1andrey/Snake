a616c4434e7344e5e2097f775fbf80a98652860c
Андрей Рочняк <ya1andrey@inbox.ru>
2023-06-08 19:02:34 +0300

    в

--- Snake/GameState.cs
+++ Snake/GameState.cs
@@ -16,6 +16,33 @@ namespace Snake
         public bool GameOver { get; private set; }
 
         private readonly LinkedList<Position> snakePosition = new LinkedList<Position>();
+        private readonly Random random = new Random();
+         
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+        }
 
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+
+            for (int c=1; c<=3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePosition.AddFirst(new Position(r, c));
+            }
+        }
+
+        private IEnumerable<Position>EmptyPositions()
+        {
+            for(int r=0; r<Rows; r++)
+            {
+
+            }
+        }
     }
 }
