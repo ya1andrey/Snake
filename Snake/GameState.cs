c90c9f940182b7eaf8da9556ad7a8e737ed8bd9c
Андрей Рочняк <ya1andrey@inbox.ru>
2023-06-08 16:36:10 +0300

    пишу

---
+++ Snake/GameState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> snakePosition = new LinkedList<Position>();
+
+    }
+}
