9dcce4ec84b386a6359930ef3f47df85ab792dbb
Андрей Рочняк <ya1andrey@inbox.ru>
2023-06-20 16:18:58 +0300

    окн

---
+++ Snake/Images.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Snake
+{
+    public static class Images
+    {
+        public readonly static ImageSource body = LoadImage("body.png");
+        public readonly static ImageSource cell = LoadImage("cell.png");
+        public readonly static ImageSource deadbody = LoadImage("deadbody.png");
+        public readonly static ImageSource deadhead = LoadImage("deadhead.png");
+        public readonly static ImageSource deadtail = LoadImage("deadtail.png");
+        public readonly static ImageSource food = LoadImage("food.png");
+        public readonly static ImageSource head = LoadImage("head.png");
+        public readonly static ImageSource tail = LoadImage("tail.png");
+
+        private static ImageSource LoadImage(string fileName)
+        {
+            return new BitmapImage(new Uri($"Assets/{fileName}", UriKind.Relative));
+        }
+
+    }
+}
--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -15,14 +15,77 @@ using System.Windows.Shapes;
 
 namespace Snake
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<GridValue, ImageSource> gridValueToImages = new Dictionary<GridValue, ImageSource>()
+        {
+            { GridValue.Empty, Images.cell },
+            { GridValue.Snake, Images.body },
+            { GridValue.Food, Images.food },
+        };
+
+        private readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImages;
+        private GameState gameState;
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetupGrid();
+            gameState = new GameState(rows, cols);
+        }
+
+        
+
+
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.cell
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+
+                }
+            }
+            return images;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Draw();
+        }
+        private void Window_KeyDown(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void Draw()
+        {
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    GridValue gridVal = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValueToImages[gridVal];
+                }
+            }
         }
     }
 }
